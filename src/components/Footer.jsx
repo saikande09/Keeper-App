3392e3ed532c3861b3dd75d519e54f2c04508cef
Sai Krishna <saikrishnakande01@gmail.com>
2024-03-02 15:47:28 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import HighlightIcon from '@mui/icons-material/Highlight';
+function Header(){
+    return (
+        <header>
+            <h1>
+            <HighlightIcon />
+            Keeper App</h1>
+        </header>
+    );
+}
+export default Header;
